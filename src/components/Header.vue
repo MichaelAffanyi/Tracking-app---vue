f00ab80acdce8dcddb9f06fd4cc4c07299832b10
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-03-21 13:22:24 +0000

    development

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button 
+            @btn-click="$emit('btn-click')" 
+            :text="showAddTask ? 'close' : 'Add Task'" 
+            :color="showAddTask ? 'red' : 'green'"
+        />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    },
+    emits: ['btn-click']
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
